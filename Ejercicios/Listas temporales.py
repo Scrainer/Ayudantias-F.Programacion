f8602f1b1718c5d3e6f345004bf6490838e52d4a
Scrainer <cesarcarlier@hotmail.com>
2017-11-16 11:56:36 -0500

    3era Ayudantia

--- Ejercicios/Listas temporales.py
+++
@@ -1,8 +0,0 @@
-calificaciones=[]
-for i in range(3):
-    notasAlumnos=[]
-    nota1=10
-    nota2=8
-    notasAlumnos.append(nota1)
-    notasAlumnos.append(nota2)
-    calificaciones.append(notasAlumnos)
