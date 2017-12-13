a83a57b747f3d8261566e7a8165a5eea4fc95022
Scrainer <cesarcarlier@hotmail.com>
2017-12-13 12:32:20 -0500

    Ayudantias / Funciones2

--- Ejercicios/funciones - Matrices.py
+++ Ejercicios/funciones - Matrices.py
@@ -6,9 +6,27 @@ def media(matriz):
             suma+=matriz[i,j]
             contador+=1
     return (suma/contador)
+def numMayores(matriz):
+    cantidad=0
+    promedio=media(matriz)
+    for i in range(0,len(matriz[:,0])):
+        for j in range(0,len(matriz[0,:])):
+            if matriz[i,j]>promedio:
+                cantidad+=1
+    return cantidad
+def numMenores(matriz):
+    cantidad = 0
+    promedio = media(matriz)
+    for i in range(0, len(matriz[:, 0])):
+        for j in range(0, len(matriz[0, :])):
+            if matriz[i, j] < promedio:
+                cantidad += 1
+    return cantidad
 
 import numpy as np
 m=np.array([[1,2,3,4],
             [2,3,4,5],
             [3,4,5,6]])
+print(m)
 print(media(m))
+print(numMayores(m))
